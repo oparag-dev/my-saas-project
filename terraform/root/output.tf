231308f020edf378f8ed457e29071cf85f434f68
oparag-dev <oparagospel001@gmail.com>
2026-03-04 16:37:35 +0100

    updated main.tf file

---
+++ terraform/root/output.tf
@@ -0,0 +1,19 @@
+output "lambda_arn" {
+  value = module.lambda.lambda_arn
+}
+
+output "dynamodb_table_name" {
+  value = module.dynamodb.table_name
+}
+
+output "api_gateway_url" {
+  value = module.api_gateway.api_endpoint
+}
+
+output "s3_audit_bucket" {
+  value = module.s3.bucket_name
+}
+
+output "cognito_user_pool_id" {
+  value = module.cognito.user_pool_id
+}
